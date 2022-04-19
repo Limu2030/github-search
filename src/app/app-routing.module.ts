48cdcb2d22cefe7070c61c2a10e95221bcc093f2
limu2030 <limu2030@gmail.com>
2022-04-20 00:07:53 +0300

    check routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    component
+  }
   // {
   //   path: '',
   //   component:
