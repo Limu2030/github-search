b8def57ca6fe5c38547c93788caa1c050f1af717
limu2030 <limu2030@gmail.com>
2022-04-20 00:10:26 +0300

    debugging the service class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    component
-  }
+  {}
   // {
   //   path: '',
   //   component:
