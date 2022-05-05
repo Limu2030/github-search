a80b2e7ced8bf387ced0f71ace647d71711a47a9
limu2030 <limu2030@gmail.com>
2022-05-05 16:05:53 +0300

    creates directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HttpClientModule} from '@angular/common/http';
+import { FontDirective } from './font.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FontDirective
   ],
   imports: [
     BrowserModule,
