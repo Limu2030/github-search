0d521507f71ca4d0efcaa1b6f3816e4b71f4fa65
limu2030 <limu2030@gmail.com>
2022-05-05 16:20:55 +0300

    ft:added getWord logic in LastPipe class

--- src/app/pipe/last.pipe.ts
+++ src/app/pipe/last.pipe.ts
@@ -1,12 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
 @Pipe({
   name: 'last'
 })
 export class LastPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): string {
+    var wordsLength = value.length;
+    var getWord =''
+    for(var i=0;i<wordsLength;i++){
+      if(value.charAt(i)===" "){
+        getWord=''
+      } else if(value.charAt(i)!==" "){
+        getWord = getWord + value.charAt(i);
+      }
+    }
+    return getWord;
   }
-
 }
+
+
